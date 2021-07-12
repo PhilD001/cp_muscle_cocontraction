6f4a3ea2866894f42068ce1d752649ca95026bdf
DESKTOP-F56CEDD\sahar <sahar.mohammadyari.gharehbolagh@umontreal.ca>
2021-07-11 23:30:40 -0400

    update bmeh_find_good_emg

--- code/bmech_find_good_emg.m
+++ code/bmech_find_good_emg.m
@@ -1,7 +1,6 @@
-function bmech_find_good_emg(fld)
+function[data2]= bmech_find_good_emg(fld)
 
-% ch={'LVM';'LTibAnt';'LGM';'LHam';'LRF';'RVM';'RTibAnt';'RGM';'RHam';'RRF'};
-% folder 3074_1 was empty from beginning
+fld=uigetfolder;
 fl =engine('path',fld,'ext','.zoo');
 data2 =struct();
 figure;
@@ -10,24 +9,24 @@ s1=subplot(2,6,1);
 s2=subplot(2,6,2);
 s3=subplot(2,6,3); 
 s4=subplot(2,6,4);
-s5=subplot(2,6,5);
-s6=subplot(2,6,6);
-s7=subplot(2,6,7);
-s8=subplot(2,6,8);
-s9=subplot(2,6,9);
-s10=subplot(2,6,10);
+% s5=subplot(2,6,5);
+% s6=subplot(2,6,6);
+% s7=subplot(2,6,7);
+% s8=subplot(2,6,8);
+
 for i=1:length(fl)
 data = zload(fl{i});
+fl{i};
 data2.(['T' num2str(i)])=data;
  
-%  subplot(s1);plot(data.LVM_normalized .line); title('LVM');hold on;
- subplot(s2);plot(data.LTibAnt_filthigh_filtlow.line); title('LTibAnt');hold on; 
- subplot(s3);plot(data.LGM_filthigh_filtlow.line); title('LGM');hold on;
- subplot(s4);plot(data.LHam_filthigh_filtlow.line);title('LHam');hold on; 
- subplot(s5);plot(data.LRF_filthigh_filtlow.line); title('LRF');hold on;
-%  subplot(s6);plot(data.RVM_normalized .line);title('RVM');hold on;  
- subplot(s7);plot(data.RTibAnt_filthigh_filtlow.line); title('RTibAnt');hold on; 
- subplot(s8);plot(data.RGM_filthigh_filtlow.line);title('RGM');hold on;  
- subplot(s9);plot(data.RHam_filthigh_filtlow.line);title('RHam');hold on;  
- subplot(s10);plot(data.RRF_filthigh_filtlow.line);title('RRF');hold on; 
+
+ subplot(s1);plot(data.LTibAnt_normalized.line); title('LTibAnt');hold on; 
+ subplot(s2);plot(data.LGM_normalized.line); title('LGM');hold on;
+ subplot(s3);plot(data.LHam_normalized.line);title('LHam');hold on; 
+ subplot(s4);plot(data.LRF_normalized.line); title('LRF');hold on;
+%  subplot(s5);plot(data.RTibAnt_normalized.line); title('RTibAnt');hold on; 
+%  subplot(s6);plot(data.RGM_normalized.line);title('RGM');hold on;  
+%  subplot(s7);plot(data.RHam_normalized.line);title('RHam');hold on;  
+%  subplot(s8);plot(data.RRF_normalized.line);title('RRF');hold on; 
+
  end
