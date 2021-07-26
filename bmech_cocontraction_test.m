1e9478741439d1fe24efa786ede702a52157091e
Vishnu Deep Chandran <vc268@njit.edu>
2021-07-26 19:04:03 -0400

    bmech_cocontraction_test: Input args update
    1) removed side as optional input
    2) added functionality to accept side prefix in pairs args
    3) updated optional input parameter handling

--- bmech_cocontraction_test.m
+++ bmech_cocontraction_test.m
@@ -5,12 +5,16 @@ function bmech_cocontraction_test(fld,pairs,varargin)
 % ARGUMENTS
 %  fld         ...   folder to operate on
 %  pairs       ...   Names of muscle pairs (cell array of strings).
-%                    Default = {'VM_MG','VM_MH','VL_LG','VL_LH'}
+%                      Prefix side L or R at begining of string input.
+%                      Example = {'LTibAnt_GM','LRF_Ham'} computes
+%                      co-contaction for LTibAnt and LGM, and LRF
+%                      and LHam muscle pairs.
 %*NEW* OPTIONAL AGRUMENT
-%  sides    ...   Prefix for limb side. Default = {'R','L'}
-%  method         ...   Choice of algorithm to use.
+%  'method'         ...   Choice of algorithm to use.
 %                       Default :'Rudolph'
 %                       Other choices :'Falconer' and 'Lo2017'.
+% 'events'   ...   pair of global events as cell array of strings.
+%                 Exampl = {'Left_FootStrike1','Left_FootStrike2'}
 % NOTES
 % - See cocontraction_line for co-contraction computational approach
 %
@@ -21,34 +25,52 @@ function bmech_cocontraction_test(fld,pairs,varargin)
 cd(fld)
 fl = engine('path',fld,'extension','zoo');
 
+method_exists = false;
+events_exists = false;
+evts ={''};
 
-if nargin==2
-    method_exists = false;
-    sides = {'R','L'};
+sides = {};
+for i=1: length(pairs)
+    side = pairs{1,i}(1);
+    sides{1,i} =side;
     
-elseif nargin==3
-    if ischar(varargin{1})
-        method_exists = true;
-        sides = {'R','L'};
-        method = varargin{1};
-    elseif iscell(varargin{1})
-        method_exists = false;
-        sides = varargin{1};
+    pairs{1,i} = pairs{1,i}(2:end);
+end
+sides = unique(sides);
+
+if nargin <2
+    error('Check Input')
+elseif nargin ==3
+    error('Check Input')
+elseif nargin>3
+    for i = 1:2:nargin-2
+        
+        switch varargin{i}
+            
+            case 'method'
+                method = varargin{i+1};
+                method_exists = true;
+                
+            case 'events'
+                evts = varargin{i+1};
+                events_exists =true;
+                
+        end
     end
-    
-elseif nargin==4
-    method_exists = true;
-    sides = varargin{1};
-    method = varargin{2};
 end
 
 
 for i = 1:length(fl)
     data = zload(fl{i});
+    
     batchdisp(fl{i},'computing co-contraction');
     
-    if method_exists
+    if method_exists&&events_exists
+        data = cocontraction_data_test(data,pairs,sides,method,evts);
+    elseif method_exists
         data = cocontraction_data_test(data,pairs,sides,method);
+    elseif events_exists
+        data = cocontraction_data_test(data,pairs,sides,evts);
     else
         data = cocontraction_data_test(data,pairs,sides);
     end
