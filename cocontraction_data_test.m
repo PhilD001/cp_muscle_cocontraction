eea659946a0ccbabc0634229e2932f4aab9ac593
Vishnu Deep Chandran <vc268@njit.edu>
2021-07-26 19:10:53 -0400

    cocontraction_data_test: added events args
    1) added functionality to estimate co-contraction based event pairs
    2) added functionality to eliminate overwriting existing channel and events
    -line data will be updated if channel exists and
    -append new event data

--- cocontraction_data_test.m
+++ cocontraction_data_test.m
@@ -23,19 +23,69 @@ function data = cocontraction_data_test(data,muscle_pairs,varargin)
 % end
 
 n= nargin; % number of input argument
+method_exists =false;
+events_exists =false;
+
 switch n
     case 3
         sides = varargin{1};
-        method_exists =false;
         
     case 4
         sides = varargin{1};
+        if ischar(varargin{2})
+            method = varargin{2};
+            method_exists =true;
+        elseif iscell(varargin{2})
+            evts =varargin{2};
+            if length(evts)==2
+                events_exists =true;
+                evts_tag = [evts{1, 1} '_to_' evts{1, 2}];
+                
+                evt1 = evts{1, 1};
+                evt2 = evts{1, 2};
+                
+                if ~isempty(findfield(data,evt1)) && ~isempty(findfield(data,evt2))
+                    data_prt = partition_data(data,evt1,evt2);
+                else
+                    disp('Invalid Event(s) provided: Ignoring event(s) and considering complete signal')
+                    events_exists =false;
+                end
+                
+            else
+                disp('Event(s) not provided: Ignoring event(s) and considering complete signal')
+                events_exists =false;
+            end
+        end
+        
+    case 5
+        sides = varargin{1};
         method = varargin{2};
         method_exists =true;
+        evts =varargin{3};
+        
+        if length(evts)==2
+            events_exists =true;
+            evts_tag = [evts{1, 1} '_to_' evts{1, 2}];
+            
+            evt1 = evts{1, 1};
+            evt2 = evts{1, 2};
+            
+            if ~isempty(findfield(data,evt1)) && ~isempty(findfield(data,evt2))
+                data_prt = partition_data(data,evt1,evt2);
+            else
+                disp('Invalid Event(s) provided: Ignoring event(s) and considering complete signal')
+                events_exists =false;
+            end
+            
+        else
+            disp('Event(s) not provided: Ignoring event(s) and considering complete signal')
+            events_exists =false;
+        end
         
     otherwise
         disp('Error: check input arguments/ not all input arguments have to provided')
 end
+
 
 for i = 1:length(muscle_pairs)
     muscles = strsplit(muscle_pairs{i},'_');
@@ -44,22 +94,60 @@ for i = 1:length(muscle_pairs)
         %         muscle1 = data.([sides{j},muscles{1}]).line;                       %       lines need to be uncommented in a normal work flow
         %         muscle2 = data.([sides{j},muscles{2}]).line;
         
-        muscle1 = data.([sides{j},muscles{1},'_normalized']).line;               %       lines need to be commented out. In a normal work flow the
-        muscle2 = data.([sides{j},muscles{2},'_normalized']).line;               %       muscles will be already replaced with data that's normalized
+        if ~isfield(data,[sides{j},muscles{1}])||~isfield(data,[sides{j},muscles{2}])
+            error('Invaid muscle(s) / muscle(s) do not exist')
+        end
+        
+        if ~isfield(data,[sides{j},muscles{1},'_normalized'])||~isfield(data,[sides{j},muscles{2},'_normalized'])
+            error('EMG channel not normalized')
+        end
+        
+        if events_exists
+            muscle1 = data.([sides{j},muscles{1},'_normalized']).line;
+            muscle2 = data.([sides{j},muscles{2},'_normalized']).line;
+            
+            muscle1_prt = data_prt.([sides{j},muscles{1},'_normalized']).line;
+            muscle2_prt = data_prt.([sides{j},muscles{2},'_normalized']).line;
+            
+            ch_name = [sides{j},muscles{1},'_',muscles{2}];
+            
+        else
+            muscle1 = data.([sides{j},muscles{1},'_normalized']).line;
+            muscle2 = data.([sides{j},muscles{2},'_normalized']).line;
+            
+            ch_name = [sides{j},muscles{1},'_',muscles{2}];
+            
+        end
         
         if method_exists
             disp(['computing co-contraction for muscles ',sides{j},muscles{1},' and ',sides{j},muscles{2},' using ',method])
             
             [r,r_val] = cocontraction_line_test(muscle1,muscle2,method);
-            data = addchannel_data(data,[sides{j},muscles{1},'_',muscles{2}],r,'Analog');
-            if r_val~=0
-                data.([sides{j},muscles{1},'_',muscles{2}]).event.co_contraction_value= [1,r_val,0];
+            
+            if ~isfield(data,[ch_name '_' method])
+                data = addchannel_data(data,[ch_name '_' method],r,'Analog');
+            else
+                disp('channel already exists: updating channel')
+                data.([ch_name '_' method]).line =r;
             end
+            
+            if r_val~=0 &&events_exists
+                [~,r_val] = cocontraction_line_test(muscle1_prt,muscle2_prt,method);
+                data.([ch_name '_' method]).event.(['co_contraction_value' '_from_' evts_tag])= [1,r_val,0];
+            elseif r_val~=0
+                data.([ch_name '_' method]).event.co_contraction_value= [1,r_val,0];
+            end
+            
         else
             disp(['computing co-contraction for muscles ',sides{j},muscles{1},' and ',sides{j},muscles{2},' using Default:Rudolph'])
             
             r = cocontraction_line_test(muscle1,muscle2);
-            data = addchannel_data(data,[sides{j},muscles{1},'_',muscles{2}],r,'Analog');
+            if ~isfield(data,[ch_name '_Rudolph'])
+                data = addchannel_data(data,[ch_name '_Rudolph'],r,'Analog');
+            else
+                disp('channel already exists: updating channel')
+                data.([ch_name '_Rudolph']).line =r;
+            end
             
         end
         
