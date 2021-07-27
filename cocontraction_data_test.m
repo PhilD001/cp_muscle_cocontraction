1c85e4cdf035677adcdc130b2788b46aee476bd7
Vishnu Deep Chandran <vc268@njit.edu>
2021-07-27 16:14:45 -0400

    cocontraction_data_test: updated error handling
    1) additional checks in place to validate event args
    2) defaults to entire cycle in case of exception

--- cocontraction_data_test.m
+++ cocontraction_data_test.m
@@ -6,7 +6,6 @@ function data = cocontraction_data_test(data,muscle_pairs,varargin)
 %  data     ...   zoo struct
 %  pairs    ...   Names of muscle pairs (cell array of strings).
 % OPTIONAL ARGUMENTS
-%  sides    ...   Prefix for limb side. Default = {'R','L'}
 %  method     ...   Choice of algorithm to use.
 %                   Default :'Rudolph',
 %                   Other choices :'Falconer' and 'Lo2017'.
@@ -23,20 +22,20 @@ function data = cocontraction_data_test(data,muscle_pairs,varargin)
 % end
 
 n= nargin; % number of input argument
-method_exists =false;
-events_exists =false;
 
 switch n
-    case 3
-        sides = varargin{1};
+    case 2
+        method_exists =false;
+        events_exists =false;
         
-    case 4
-        sides = varargin{1};
-        if ischar(varargin{2})
-            method = varargin{2};
+    case 3
+        if ischar(varargin{1})
+            method = varargin{1};
             method_exists =true;
-        elseif iscell(varargin{2})
-            evts =varargin{2};
+            events_exists =false;
+        elseif iscell(varargin{1})
+            evts =varargin{1};
+            method_exists =false;
             if length(evts)==2
                 events_exists =true;
                 evts_tag = [evts{1, 1} '_to_' evts{1, 2}];
@@ -45,23 +44,36 @@ switch n
                 evt2 = evts{1, 2};
                 
                 if ~isempty(findfield(data,evt1)) && ~isempty(findfield(data,evt2))
-                    data_prt = partition_data(data,evt1,evt2);
+                    
+                    fields = fieldnames(data);
+                    SACR_field =fields(strcmp(fields,'SACR'));
+                    
+                    if isempty(SACR_field)
+                        SACR_field= fields(strcmp(fields,'SACR_x'));
+                    end
+                    
+                    if data.(SACR_field{1}).event.(evt1)(1)<data.(SACR_field{1}).event.(evt2)(1) && (data.(SACR_field{1}).event.(evt2)(1)-data.(SACR_field{1}).event.(evt1)(1))< length(data.(SACR_field{1}).line)
+                        data_prt = partition_data(data,evt1,evt2);
+                    else
+                        disp('issue with event 1 and event 2: Ignoring event(s) and considering complete signal')
+                        events_exists =false;
+                    end
+                    
                 else
-                    disp('Invalid Event(s) provided: Ignoring event(s) and considering complete signal')
+                    disp('event(s) do not exist: Ignoring event(s) and considering complete signal')
                     events_exists =false;
                 end
                 
             else
-                disp('Event(s) not provided: Ignoring event(s) and considering complete signal')
+                disp('events 2 not provided: Ignoring event(s) and considering complete signal')
                 events_exists =false;
             end
         end
         
-    case 5
-        sides = varargin{1};
-        method = varargin{2};
+    case 4
+        method = varargin{1};
         method_exists =true;
-        evts =varargin{3};
+        evts =varargin{2};
         
         if length(evts)==2
             events_exists =true;
@@ -71,87 +83,102 @@ switch n
             evt2 = evts{1, 2};
             
             if ~isempty(findfield(data,evt1)) && ~isempty(findfield(data,evt2))
-                data_prt = partition_data(data,evt1,evt2);
+                
+                fields = fieldnames(data);
+                SACR_field =fields(strcmp(fields,'SACR'));
+                
+                if isempty(SACR_field)
+                    SACR_field= fields(strcmp(fields,'SACR_x'));
+                end
+                
+                if data.(SACR_field{1}).event.(evt1)(1)<data.(SACR_field{1}).event.(evt2)(1) && (data.(SACR_field{1}).event.(evt2)(1) - data.(SACR_field{1}).event.(evt1)(1))< length(data.(SACR_field{1}).line)
+                    data_prt = partition_data(data,evt1,evt2);
+                else
+                    disp( 'issue with event 1 and event 2: Ignoring event(s) and considering complete signal')
+                    events_exists =false;
+                end
+                
             else
-                disp('Invalid Event(s) provided: Ignoring event(s) and considering complete signal')
+                disp('event(s) do not exist: Ignoring event(s) and considering complete signal')
                 events_exists =false;
             end
             
         else
-            disp('Event(s) not provided: Ignoring event(s) and considering complete signal')
+            disp('events 2 not provided: Ignoring event(s) and considering complete signal')
             events_exists =false;
         end
         
     otherwise
-        disp('Error: check input arguments/ not all input arguments have to provided')
+        disp('Error: check input arguments \ not all input arguments have to provided')
 end
 
 
 for i = 1:length(muscle_pairs)
-    muscles = strsplit(muscle_pairs{i},'_');
+    muscles = strsplit(muscle_pairs{i},'-');
     
-    for j = 1:length(sides)
-        %         muscle1 = data.([sides{j},muscles{1}]).line;                       %       lines need to be uncommented in a normal work flow
-        %         muscle2 = data.([sides{j},muscles{2}]).line;
+    %         muscle1 = data.([muscles{1}]).line;
+    %         muscle2 = data.([muscles{2}]).line;
+    
+    if ~isfield(data,[muscles{1}])||~isfield(data,[muscles{2}])
+        error('invalid muscle(s) / muscle(s) do not exist')
+    end
+    
+    if ~isfield(data,[muscles{1},'_normalized'])||~isfield(data,[muscles{2},'_normalized'])
+        error('EMG channel not normalized')
+    end
+    
+    if events_exists
+        muscle1 = data.([muscles{1},'_normalized']).line;
+        muscle2 = data.([muscles{2},'_normalized']).line;
         
-        if ~isfield(data,[sides{j},muscles{1}])||~isfield(data,[sides{j},muscles{2}])
-            error('Invaid muscle(s) / muscle(s) do not exist')
-        end
+        muscle1_prt = data_prt.([muscles{1},'_normalized']).line;
+        muscle2_prt = data_prt.([muscles{2},'_normalized']).line;
         
-        if ~isfield(data,[sides{j},muscles{1},'_normalized'])||~isfield(data,[sides{j},muscles{2},'_normalized'])
-            error('EMG channel not normalized')
-        end
+        ch_name = [muscles{1},'_',muscles{2}];
         
-        if events_exists
-            muscle1 = data.([sides{j},muscles{1},'_normalized']).line;
-            muscle2 = data.([sides{j},muscles{2},'_normalized']).line;
-            
-            muscle1_prt = data_prt.([sides{j},muscles{1},'_normalized']).line;
-            muscle2_prt = data_prt.([sides{j},muscles{2},'_normalized']).line;
-            
-            ch_name = [sides{j},muscles{1},'_',muscles{2}];
-            
+    else
+        muscle1 = data.([muscles{1},'_normalized']).line;
+        muscle2 = data.([muscles{2},'_normalized']).line;
+        
+        ch_name = [muscles{1},'_',muscles{2}];
+        
+    end
+    
+    if method_exists
+        disp(['computing co-contraction for muscles ',muscles{1},' and ',muscles{2},' using ',method])
+        
+        [r,r_val] = cocontraction_line_test(muscle1,muscle2,method);
+        
+        if ~isfield(data,[ch_name '_' method])
+            data = addchannel_data(data,[ch_name '_' method],r,'Analog');
         else
-            muscle1 = data.([sides{j},muscles{1},'_normalized']).line;
-            muscle2 = data.([sides{j},muscles{2},'_normalized']).line;
-            
-            ch_name = [sides{j},muscles{1},'_',muscles{2}];
-            
+            disp('channel already exists: updating channel')
+            data.([ch_name '_' method]).line =r;
         end
         
-        if method_exists
-            disp(['computing co-contraction for muscles ',sides{j},muscles{1},' and ',sides{j},muscles{2},' using ',method])
-            
-            [r,r_val] = cocontraction_line_test(muscle1,muscle2,method);
-            
-            if ~isfield(data,[ch_name '_' method])
-                data = addchannel_data(data,[ch_name '_' method],r,'Analog');
-            else
-                disp('channel already exists: updating channel')
-                data.([ch_name '_' method]).line =r;
-            end
-            
-            if r_val~=0 &&events_exists
-                [~,r_val] = cocontraction_line_test(muscle1_prt,muscle2_prt,method);
-                data.([ch_name '_' method]).event.(['co_contraction_value' '_from_' evts_tag])= [1,r_val,0];
-            elseif r_val~=0
-                data.([ch_name '_' method]).event.co_contraction_value= [1,r_val,0];
-            end
-            
+        if r_val~=0 &&events_exists
+             disp(['from ', evts_tag])
+            [~,r_val] = cocontraction_line_test(muscle1_prt,muscle2_prt,method);
+            data.([ch_name '_' method]).event.(['co_contraction_value' '_from_' evts_tag])= [1,r_val,0];
+        elseif r_val~=0
+            disp(['for entire signal'])
+            data.([ch_name '_' method]).event.co_contraction_value= [1,r_val,0];
+        end
+        
+    else
+        disp(['computing co-contraction for muscles ',muscles{1},' and ',muscles{2},' using Default:Rudolph'])
+        disp(['for entire signal'])
+        r = cocontraction_line_test(muscle1,muscle2);
+        if ~isfield(data,[ch_name '_Rudolph'])
+            data = addchannel_data(data,[ch_name '_Rudolph'],r,'Analog');
         else
-            disp(['computing co-contraction for muscles ',sides{j},muscles{1},' and ',sides{j},muscles{2},' using Default:Rudolph'])
-            
-            r = cocontraction_line_test(muscle1,muscle2);
-            if ~isfield(data,[ch_name '_Rudolph'])
-                data = addchannel_data(data,[ch_name '_Rudolph'],r,'Analog');
-            else
-                disp('channel already exists: updating channel')
-                data.([ch_name '_Rudolph']).line =r;
-            end
-            
+            disp('channel already exists: updating channel')
+            data.([ch_name '_Rudolph']).line =r;
         end
         
     end
+    
+    
     
 end
 end
