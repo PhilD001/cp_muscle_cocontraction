a87468b67a2412fe1229123e96204e0b8b3e9584
DESKTOP-F56CEDD\sahar <sahar.mohammadyari.gharehbolagh@umontreal.ca>
2021-08-01 16:55:28 -0400

    update

--- code/bmech_find_good_emg.m
+++ code/bmech_find_good_emg.m
@@ -9,24 +9,18 @@ s1=subplot(2,6,1);
 s2=subplot(2,6,2);
 s3=subplot(2,6,3); 
 s4=subplot(2,6,4);
-% s5=subplot(2,6,5);
-% s6=subplot(2,6,6);
-% s7=subplot(2,6,7);
-% s8=subplot(2,6,8);
+
 
 for i=1:length(fl)
 data = zload(fl{i});
-fl{i}
+fl{i};
 data2.(['T' num2str(i)])=data;
  
 
- subplot(s1);plot(data.LTibAnt_normalized.line); title('LTibAnt');hold on; 
- subplot(s2);plot(data.LGM_normalized.line); title('LGM');hold on;
- subplot(s3);plot(data.LHam_normalized.line);title('LHam');hold on; 
- subplot(s4);plot(data.LRF_normalized.line); title('LRF');hold on;
-%  subplot(s5);plot(data.RTibAnt_normalized.line); title('RTibAnt');hold on; 
-%  subplot(s6);plot(data.RGM_normalized.line);title('RGM');hold on;  
-%  subplot(s7);plot(data.RHam_normalized.line);title('RHam');hold on;  
-%  subplot(s8);plot(data.RRF_normalized.line);title('RRF');hold on; 
+ subplot(s1);plot(data.L_Tib_Ant_normalized.line); title('L_Tib_Ant');hold on; 
+ subplot(s2);plot(data.L_Gast_normalized.line); title('L_Gast');hold on;
+ subplot(s3);plot(data.L_Hams_normalized.line);title('L_Hams');hold on; 
+ subplot(s4);plot(data.L_Rect_normalized.line); title('L_Rect');hold on;
+
 
  end
